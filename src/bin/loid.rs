6ab9b57cd31fa721e4c4576bfb43aaac7be60f45
Zachary <me@dusterthefirst.com>
2022-02-06 13:33:26 +0100

    Reconstruct signal from FFT spectrum

--- src/bin/loid.rs
+++ src/bin/loid.rs
@@ -1,17 +1,19 @@
-use std::{sync::Arc, thread};
+use std::{iter, sync::Arc, thread};
 
 use eframe::{
     egui::{
-        plot::{Bar, BarChart, Legend, Plot, Points, Value, Values},
+        plot::{Bar, BarChart, Legend, Plot, Points, VLine, Value, Values},
         Button, CentralPanel, CtxRef,
     },
     epi::{App, Frame},
     NativeOptions,
 };
-use microfft::real::rfft_4096;
+use microfft::{complex::cfft_16384, real::rfft_16384};
 use num_complex::Complex;
-use rodio::{buffer::SamplesBuffer, source::Buffered, OutputStream, Sink, Source};
+use rodio::{buffer::SamplesBuffer, OutputStream, Sink, Source};
 use speaky::{load_language, setup_tts, synthesize};
+
+const N: usize = 16384;
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -22,49 +24,72 @@ fn main() {
 
     let mut engine = setup_tts(resources);
 
-    let speech = synthesize(&mut engine, "Some").buffered();
+    let speech = synthesize(&mut engine, "Some Body Once");
 
-    // let sample_rate = speech.sample_rate();
-    let samples: Vec<f32> = speech.clone().convert_samples().collect();
+    // let speech = SineWave::new(20.0).take_duration(Duration::from_secs_f32(1.0 / 10.0));
 
-    let mut samples: [f32; 4096] = samples[..4096].try_into().unwrap();
+    let sample_rate = speech.sample_rate();
+    let samples: Vec<f32> = speech.convert_samples().collect();
 
-    let spectrum = rfft_4096(&mut samples);
+    let half_spectrum = {
+        assert!(samples.len() >= N, "Too few samples");
 
-    // TODO: more idiomatic way?
-    let spectrum = unsafe { std::mem::transmute(samples) };
+        let mut fixed_sized_samples: Box<[f32; N]> = Box::new([0.0; N]);
+        fixed_sized_samples.copy_from_slice(&samples[..N]);
 
-    // dbg!(amplitudes.iter().max());
+        let fixed_sized_samples = Box::leak(fixed_sized_samples);
 
-    // let pow_2_len = 1 << (usize::BITS - samples.len().leading_zeros() - 1);
+        let spectrum = rfft_16384(fixed_sized_samples);
 
-    // let hann_window = hann_window(&samples[..pow_2_len.min(4096)]);
+        // This saves a large copy
+        unsafe { Box::from_raw(spectrum) }
+    };
 
-    // let spectrum_hann_window = samples_fft_to_spectrum(
-    //     &hann_window,
-    //     sample_rate,
-    //     FrequencyLimit::All,
-    //     None,
-    // )
-    // .unwrap();
+    let full_spectrum = {
+        // The real-valued coefficient at the Nyquist frequency
+        // is packed into the imaginary part of the DC bin.
+        let real_at_nyquist = half_spectrum[0].im;
+        let dc = half_spectrum[0].re;
 
-    // dbg!(spectrum_hann_window.max());
+        let half_spectrum = half_spectrum.iter().skip(1).copied();
 
-    // for (fr, fr_val) in spectrum_hann_window.data().iter() {
-    //     println!("{}Hz => {}", fr, fr_val)
-    // }
+        iter::once(Complex::new(dc, 0.0))
+            .chain(half_spectrum.clone())
+            .chain(iter::once(Complex::new(real_at_nyquist, 0.0)))
+            .chain(half_spectrum.map(|complex| complex.conj()).rev())
+            .collect::<Vec<_>>()
+    };
 
-    // sink.append(speech);
+    let reconstructed_samples = {
+        let spectrum_conjugate = full_spectrum
+            .iter()
+            .map(|complex| Complex::new(complex.im, complex.re));
 
-    // sink.sleep_until_end();
+        let mut fixed_sized_spectrum: Box<[Complex<f32>; N]> =
+            Box::new([Complex::new(0.0, 0.0); N]);
+
+        // Collect iterator into existing buffer
+        for (complex_in, complex_out) in spectrum_conjugate.zip(fixed_sized_spectrum.iter_mut()) {
+            *complex_out = complex_in;
+        }
+
+        let samples = cfft_16384(&mut fixed_sized_spectrum);
+
+        samples
+            .iter()
+            .map(|complex| complex.im / N as f32)
+            .collect()
+    };
 
     eframe::run_native(
         Box::new(Loid {
             audio_sink: Arc::new(sink),
 
-            speech,
+            sample_rate,
+            samples,
+            reconstructed_samples,
 
-            spectrum,
+            spectrum: half_spectrum,
         }),
         NativeOptions::default(),
     )
@@ -73,9 +98,27 @@ fn main() {
 struct Loid {
     audio_sink: Arc<Sink>,
 
-    speech: Buffered<SamplesBuffer<i16>>,
+    sample_rate: u32,
 
-    spectrum: [Complex<f32>; 2048],
+    samples: Vec<f32>,
+    reconstructed_samples: Vec<f32>,
+
+    spectrum: Box<[Complex<f32>; N / 2]>,
+}
+
+impl Loid {
+    fn play(&self, samples: &[f32], frame: Frame) {
+        self.audio_sink
+            .append(SamplesBuffer::new(1, self.sample_rate, samples));
+
+        thread::spawn({
+            let audio_sink = self.audio_sink.clone();
+            move || {
+                audio_sink.sleep_until_end();
+                frame.request_repaint();
+            }
+        });
+    }
 }
 
 impl App for Loid {
@@ -83,43 +126,42 @@ impl App for Loid {
         CentralPanel::default().show(ctx, |ui| {
             ui.horizontal_wrapped(|ui| {
                 if ui
-                    .add_enabled(self.audio_sink.empty(), Button::new("Play"))
+                    .add_enabled(self.audio_sink.empty(), Button::new("Play Original"))
                     .clicked()
                 {
-                    self.audio_sink.append(self.speech.clone());
+                    self.play(self.samples.as_ref(), frame.clone());
+                }
 
-                    thread::spawn({
-                        let audio_sink = self.audio_sink.clone();
-                        let frame = frame.clone();
-                        move || {
-                            audio_sink.sleep_until_end();
-                            frame.request_repaint();
-                        }
-                    });
+                if ui
+                    .add_enabled(self.audio_sink.empty(), Button::new("Play Reconstructed"))
+                    .clicked()
+                {
+                    self.play(self.reconstructed_samples.as_ref(), frame.clone());
                 }
             });
 
             // FIXME: heavy on the iterators I think
             Plot::new("samples")
-                .height(ui.available_height() / 2.0)
+                .height(ui.available_height() / 3.0)
                 .center_y_axis(true)
                 .show(ui, |ui| {
-                    let sampling_rate = self.speech.sample_rate();
-
                     ui.points(
-                        Points::new(Values::from_values_iter({
-                            self.speech
-                                .clone()
-                                .convert_samples::<f32>()
+                        Points::new(Values::from_values_iter(
+                            self.samples
+                                .iter()
+                                .copied()
                                 .enumerate()
-                                .map(move |(n, x)| Value::new(n as f32 / sampling_rate as f32, x))
-                        }))
+                                .map(|(n, x)| Value::new(n as f32 / self.sample_rate as f32, x)),
+                        ))
                         .stems(0.0),
-                    )
+                    );
+
+                    ui.vline(VLine::new(N as f32 / self.sample_rate as f32));
+                    ui.vline(VLine::new(0.0 / self.sample_rate as f32));
                 });
 
             Plot::new("frequencies")
-                .height(ui.available_height())
+                .height(ui.available_height() / 2.0)
                 .legend(Legend::default())
                 .show(ui, |ui| {
                     let amplitudes = self.spectrum.iter().map(|complex| complex.norm());
@@ -145,6 +187,25 @@ impl App for Loid {
                         )
                         .name("Phase"),
                     );
+                });
+
+            Plot::new("reconstructed_samples")
+                .height(ui.available_height())
+                .center_y_axis(true)
+                .show(ui, |ui| {
+                    ui.points(
+                        Points::new(Values::from_values_iter(
+                            self.reconstructed_samples
+                                .iter()
+                                .copied()
+                                .enumerate()
+                                .map(|(n, x)| Value::new(n as f32 / self.sample_rate as f32, x)),
+                        ))
+                        .stems(0.0),
+                    );
+
+                    ui.vline(VLine::new(N as f32 / self.sample_rate as f32));
+                    ui.vline(VLine::new(0.0 / self.sample_rate as f32));
                 });
         });
     }
