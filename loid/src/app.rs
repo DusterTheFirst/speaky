2ff722c9443f8fc289fd9ac16204948c7a027d41
Zachary <me@dusterthefirst.com>
2022-02-22 13:09:09 +0100

    Clean some things up

--- loid/src/app.rs
+++ loid/src/app.rs
@@ -17,8 +17,8 @@ use common::{
 use eframe::{
     egui::{
         plot::{Bar, BarChart, Legend, Line, Plot, PlotUi, Points, Text, VLine, Value, Values},
-        Align2, Button, CentralPanel, Color32, CtxRef, Label, ScrollArea, SidePanel, Slider,
-        TextStyle, TopBottomPanel,
+        Align, Align2, Button, CentralPanel, Color32, CtxRef, Label, Layout, ScrollArea, SidePanel,
+        Slider, TextStyle, TopBottomPanel,
     },
     epi::{App, Frame},
 };
@@ -371,6 +371,28 @@ impl App for Application {
                     if ui.button("Panic").clicked() {
                         panic!("User initiated panic");
                     }
+                });
+
+                // About section
+                ui.separator();
+
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+
+                    ui.label("powered by ");
+                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label(" and ");
+                    ui.hyperlink_to(
+                        "eframe",
+                        "https://github.com/emilk/egui/tree/master/eframe",
+                    );
+                });
+
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+
+                    ui.label("source available on ");
+                    ui.hyperlink_to("github", "https://github.com/dusterthefirst/vocode");
                 });
             });
         });
