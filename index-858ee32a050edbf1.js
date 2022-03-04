1fabf7dc048ae691d83a9b90011de1534890ab73
DusterTheFirst <me@dusterthefirst.com>
2022-03-04 13:52:58 +0000

    Deploying to dist-loid from @ DusterTheFirst/vocode@16a496dab7ada6d753649856433c3ae57d042245 🚀

--- index-858ee32a050edbf1.js
+++ index-858ee32a050edbf1.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-24593f4648ca7ae8_bg.wasm', import.meta.url);
+        input = new URL('index-858ee32a050edbf1_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
